397520d596506318a20102acf384e767c80f024b
opilane <opilane@HP-CZC2349HTN>
2024-02-13 10:46:09 +0200

    Add project files.

---
+++ Python_Pr10/Python_Pr10.py
@@ -0,0 +1,57 @@
+﻿#2.3
+from random import *
+vanused=[]
+N=int(input("Mitu elemendi? "))
+for i in range(N):
+    vanus=randint(10,97)
+    vanused.append(vanus)
+print(vanused)
+min_=min(vanused)
+max_=max(vanused)
+sum_=sum(vanused)
+avg_=sum_/N
+print("Minimum",min_)
+print("Maksimum",max_)
+print("Kogusumma",sum_)
+print("keskminne",avg_)
+#2.2
+opilased = ['Juhan','Kati','Mario','Mario','Mati','Mati']
+uus=[]
+for nimi in opilased:
+    if nimi not in uus: #not in
+        uus.append(nimi)
+print(uus)
+
+#2.1
+print("")
+nimed=[]
+for i in range(5):
+    nimi=input("Sisestage {i+1}. nimi: ").capitalize()
+    nimed.append(nimi)
+#vim_nimi=nimi
+print("Oli: ",nimed)
+nimed.sort()
+print("Sortimise pärast",nimed)
+print("Vilmasena oli lisatud:",nimi)
+print("")
+
+vananimi=input("Mis nimi on vaja asandada? ")
+if nimed.count(vananimi)>0:
+    uusnimi=input("Mis on uus nimi? ").capitalize()
+    #for nimi in nimed:
+    #    if nimi==vananimi: 
+    #        nimed[nimi.replace(vananimi,uusnimi)]
+    i=0
+    for nimi in nimed:
+        if nimi==vananimi:
+            nimed[i]=uusnimi
+        i+=1
+
+else:
+    print(f"{vananimi} ei ole")
+print(nimed)
+
+
+
+
+
